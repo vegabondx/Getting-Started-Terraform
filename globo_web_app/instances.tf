90acfce14556d4a87d7bbbae26b2ace71aa6c623
vegabondx <abhijit.oka@hotmail.com>
2025-02-11 13:42:05 -0500

    moving to instances local

--- globo_web_app/instances.tf
+++ globo_web_app/instances.tf
@@ -1,7 +1,23 @@
 
-
+# Get data for ami
 data "aws_ssm_parameter" "amzn2_linux" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+# Saving the user data script for reuse
+locals {
+    user_data_script = <<EOF
+    #! /bin/bash
+    sudo amazon-linux-extras install -y nginx1
+    sudo service nginx start
+    aws s3 cp s3://${aws_s3_bucket.s3bucket.id}/website/index.html /home/ec2-user/index.html
+    aws s3 cp s3://${aws_s3_bucket.s3bucket.id}/website/Globo_logo_Vert.png /home/ec2-user/Globo_logo_Vert.png
+    sudo rm /usr/share/nginx/html/index.html
+    sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
+    sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png
+    EC2_DNS=`curl http://169.254.169.254/latest/meta-data/public-hostname`
+    sed -i "s/DNS_ADDRESS/$EC2_DNS/g" /usr/share/nginx/html/index.html
+EOF
 }
 
 # INSTANCES #
